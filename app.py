d04d93edafd6bdfcaba7ecf96576c6653b91cf27
daryn <daryn@hex50.com>
2020-05-23 19:39:38 -0700

    create index.html with css, jss, font links
    create static, templates folders. .env file/python module.

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+else:
+    app.debug = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# landing page route...
+@app.route('/')
+def home():
+    return render_template('index.html')
+    #return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
